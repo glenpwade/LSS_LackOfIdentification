b96a8a4a03b896ee3ac5fca70a7eadf92282f249
glenpwade <glenpwade@gmail.com>
2025-12-08 12:29:16 +1000

    After 12000 series sim

--- R/GenSimulationData.R
+++ R/GenSimulationData.R
@@ -4,7 +4,8 @@
 
 library(MTVGARCH)   # Ver. 0.9.5.7
 
-setwd("C:\\Source\\Repos\\LSS_LackOfIdentification")
+#setwd("C:\\Source\\Repos\\LSS_LackOfIdentification")
+setwd("C:\\Repos\\LSS_LackOfIdentification")
 
 set.seed(1984)
 Nr.Series <- 12000
@@ -15,44 +16,18 @@ noiseDist$name = 'Normal'
 noiseDist$mean = 0  
 noiseDist$sd = 1
 
-# Gen Data T2000: ####
-Tobs = 2000
-st = (1:Tobs)/Tobs
-shape = tvshape$single
-# Create the tv object with default parameters
-TV <- MTVGARCH::tv(st,shape)
-
-# Now override parameters with desired model specification
-TV$speedopt <- speedopt$gamma
-TV$delta0 = 0.5
-TV$pars["deltaN",1] = 1.5
-TV$pars["speedN",1] = 10
-TV$pars["locN1",1] = 0.5
-
-# Create the garch object with default parameters
-GARCH = MTVGARCH::garch(garchtype$general)
-# Now override parameters with desired model specification
-GARCH$pars["omega",1] = 0.1
-GARCH$pars["alpha",1] = 0.1
-GARCH$pars["beta",1] = 0.8
-
-# Generate series of simulated data, injected with this process:
-Ref_Data <- MTVGARCH::generateRefData(nr.series = Nr.Series,nr.obs = Tobs,tvObj = TV,garchObj = GARCH)
-saveRDS(Ref_Data,"T2000_Data.RDS")
-
 
 # Gen Data T5000: ####
+
 Tobs = 5000
 st = (1:Tobs)/Tobs
 shape = tvshape$single
 # Create the tv object with default parameters
 TV <- MTVGARCH::tv(st,shape)
-
 # Now override parameters with desired model specification
-TV$speedopt <- speedopt$gamma
 TV$delta0 = 0.5
 TV$pars["deltaN",1] = 1.5
-TV$pars["speedN",1] = 10
+TV$pars["speedN",1] = logb(10)
 TV$pars["locN1",1] = 0.5
 
 # Create the garch object with default parameters
@@ -63,7 +38,7 @@ GARCH$pars["alpha",1] = 0.1
 GARCH$pars["beta",1] = 0.8
 
 # Generate series of simulated data, injected with this process:
-Ref_Data <- MTVGARCH::generateRefData(nr.series = Nr.Series,nr.obs = Tobs,tvObj = TV,garchObj = GARCH)
+Ref_Data <- MTVGARCH::generateRefData(nr.series = Nr.Series,nr.obs = Tobs,tvObj = TV,garchObj = GARCH, corrObj = NULL,  noiseDist = noiseDist)
 saveRDS(Ref_Data,"T5000_Data.RDS")
 
 
--- R/LSS_Main.R
+++ R/LSS_Main.R
@@ -3,11 +3,13 @@
 library(tvgarch)
 library(doParallel)
 
-setwd("C:\\Source\\Repos\\LSS_LackOfIdentification")
+#setwd("C:\\Source\\Repos\\LSS_LackOfIdentification")
+setwd("C:\\Repos\\LSS_LackOfIdentification")
 
 # T2000:  ####
 
-T2000_Data <- readRDS("T2000_Data.RDS")
+T5000_Data <- readRDS("T5000_Data.RDS")
+T2000_Data <- Ref_Data[1:2000,]
 
 
 # Test 1 Series: ####
@@ -42,37 +44,43 @@ if(FALSE){
 
 # We want to store the parameters and their standard errors for both TV & GARCH
 # We'll just use a matrix with TV$pars, GARCH$pars
-
+Nr.Series <- 12000
 
 # Setup the parallel backend
-numCores <- 6
+numCores <- 10
 Sys.setenv("MC_CORES" = numCores)
 cl <- makeCluster(numCores)
 registerDoParallel(cl, cores = numCores)
 
 nr.trans <- 1
 
-results_defStartPars = foreach(i=1:1100,.combine = rbind,.inorder = FALSE,.packages = "tvgarch")%dopar%{
+results_defStartPars = foreach(i=1:Nr.Series,.combine = rbind,.inorder = FALSE,.packages = "tvgarch")%dopar%{
 
-    # Do first iteration of estimation:
+    # Using package default starting pars
     tvg_mod <- tvgarch::tvgarch(T2000_Data[,i],nr.trans,turbo = TRUE)
     # Return:
     c(tvg_mod$iter, coef(tvg_mod, "tvgarch"))
     
 }
 
+
+# Stop the parallel cluster
+stopCluster(cl)
+
+saveRDS(results_setStartPars,"T2000_Results_lss_defStartPars.RDS")
+
 # Set start Pars
 initVals <- list()
 initVals$intercept.g = 0.4  # delta0
 initVals$size = 1.0         # delta1
-#initVals$speed = 5.0      # throws an error for some reason
+initVals$speed = 2.0        # throws an error for values > 7 (suggests eta)
 initVals$location = 0.4
 #
 initVals$intercept.h = 0.1  # omega
 initVals$arch = 0.1         # alpha
 initVals$garch = 0.8         # beta
 
-results_setStartPars = foreach(i=1:1100,.combine = rbind,.inorder = FALSE,.packages = "tvgarch")%dopar%{
+results_setStartPars = foreach(i=1:Nr.Series,.combine = rbind,.inorder = FALSE,.packages = "tvgarch")%dopar%{
     
     # Do first iteration of estimation:
     tvg_mod <- tvgarch::tvgarch(T2000_Data[,i],nr.trans,initial.values = initVals,turbo = TRUE)
@@ -91,7 +99,7 @@ calcStats <- function(resultSet){
     
     resultSet <- resultSet[,c(2:8)]
     
-    biasSet <- c(0.5,1.5,10,0.5,0.1,0.1,0.5) - colMeans(resultSet)
+    biasSet <- colMeans(resultSet) - c(0.5,1.5,logb(10),0.5,0.1,0.1,0.8)
     sdSet <- c(sd(resultSet[,1]),sd(resultSet[,2]),sd(resultSet[,3]),sd(resultSet[,4]),sd(resultSet[,5]),sd(resultSet[,6]),sd(resultSet[,7]))
     
     tblResultsGt <- matrix( c(biasSet[1],sdSet[1], biasSet[2],sdSet[2], biasSet[3],sdSet[3], biasSet[4],sdSet[4]), nrow = 1, ncol = 8 )
@@ -110,23 +118,36 @@ calcStats <- function(resultSet){
     
 }
 
+
+# Analyse resuls
 results <- results_setStartPars
+results <- results_defStartPars
 
 # Diverged:
-results[results[,1]==1000, ]
+divergentRes <- NROW(results[results[,1]==1000, ])
 good_res <- results[results[,1]<1000, ]
 
 ##
 
 
-results_InclBad <- results[1:1000, ]          # First 1000, include failed models
+results_InclBad <- results[1:12000, ]          # First 1000, include failed models
 results_Valid <- results[results[,1]<1000, ]  # Filter out all models that diverged
-results_Valid <- results_Valid[1:1000,]
+results_Valid <- results_Valid[1:12000,]
 
 sillyReport <- calcStats(results_InclBad)
 stdReport <- calcStats(results_Valid)
 
+# What are the default start pars?
+tvg_mod <- tvgarch::tvgarch(T2000_Data[,1],nr.trans,turbo = TRUE,trace = TRUE)
 
+# Initial parameters for the h component
+ 
+#        intercept.h arch1 garch1
+# Value:         0.1   0.1    0.7
 
+# Initial parameters for the g component:
+    
+#     intercept.g size1 speed1 location1
+# Value:        1   0.1 2.3026    0.5002
 
 
