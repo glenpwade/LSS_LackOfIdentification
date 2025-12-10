fa7d804e34b854f48661ad4ac819b0ab0b1b1ace
gwade-integral <gwade@integral.com.au>
2025-12-10 13:34:04 +1000

    Final comparison code

--- R/LSS_Main.R
+++ R/LSS_Main.R
@@ -1,61 +1,66 @@
+# tvgarch package:
+# Initial parameters for the h component
+# 
+#        intercept.h arch1 garch1
+# Value:         0.1   0.1    0.7
+# 
+# Initial parameters for the g component:
+#     
+#        intercept.g size1 speed1 location1
+# Value:           1   0.1 2.3026    0.5002
+#
+# Note: speed default indicates that SpeedOption = eta
 
 
+# Install required Packages - once
+if(FALSE){
+    install.packages("tvgarch")
+    install.packages("doParallel")
+    install.packages("knitr")  
+}
+
+# Unload the MTVGARCH package to avoid masking issues
+try(detach("package:MTVGARCH", unload = TRUE, character.only = TRUE))
+# Load required packages
 library(tvgarch)
 library(doParallel)
 library(knitr)
 
-#setwd("C:\\Source\\Repos\\LSS_LackOfIdentification")
-setwd("C:\\Repos\\LSS_LackOfIdentification")
+# Set a working directory
+setwd("C:\\Source\\Repos\\LSS_LackOfIdentification")
+
 
 # T2000:  ####
-
-T5000_Data <- readRDS("T5000_Data.RDS")
-T2000_Data <- Ref_Data[1:2000,]
+T2000_Data <- readRDS("T2000_Data.RDS")
 
 
 # Test 1 Series: ####
 
 if(FALSE){
-    e = T2000_Data[,55]
+    e = T2000_Data[,1]
     plot(e,type='l')
-    
 
-    nr.trans <- 1
-    initVals <- list()
-    initVals$intercept.g = 0.5  # delta0
-    initVals$size = 1.5         # delta1
-    #initVals$speed = 10.0      # throws an error for some reason
-    initVals$location = 0.5
-    #
-    initVals$intercept.h = 0.1  # omega
-    initVals$arch = 0.1         # alpha
-    initVals$garch = 0.8         # beta
-    
-    tvg_mod <- tvgarch::tvgarch(e,nr.trans,initial.values = initVals, trace = TRUE)
+    tvg_mod <- tvgarch::tvgarch(e,nr.trans = 1, trace = TRUE)
     
     summary(tvg_mod)
     plot(tvg_mod)
     
-   
 }
-
 
 
 # Run Simulation:  ####
 
-# We want to store the parameters and their standard errors for both TV & GARCH
-# We'll just use a matrix with TV$pars, GARCH$pars
-Nr.Series <- 12000
+Nr.Series <- 1100    # Simulate 1100 estimations, to ensure we have 1000 that complete without error
 
 # Setup the parallel backend
-numCores <- 10
+numCores <- 6
 Sys.setenv("MC_CORES" = numCores)
 cl <- makeCluster(numCores)
 registerDoParallel(cl, cores = numCores)
 
 nr.trans <- 1
 
-results_defStartPars = foreach(i=1:Nr.Series,.combine = rbind,.inorder = FALSE,.packages = "tvgarch")%dopar%{
+results_defStartPars = foreach(i=1:Nr.Series,.combine = rbind,.inorder = TRUE,.packages = "tvgarch")%dopar%{
 
     # Using package default starting pars
     tvg_mod <- tvgarch::tvgarch(T2000_Data[,i],nr.trans,turbo = TRUE)
@@ -68,31 +73,37 @@ results_defStartPars = foreach(i=1:Nr.Series,.combine = rbind,.inorder = FALSE,.
 # Stop the parallel cluster
 stopCluster(cl)
 
-saveRDS(results_setStartPars,"T2000_Results_lss_defStartPars.RDS")
+saveRDS(results_defStartPars,"T2000_Results_lss_defStartPars.RDS")
 
-# Set start Pars
-initVals <- list()
-initVals$intercept.g = 0.4  # delta0
-initVals$size = 1.0         # delta1
-initVals$speed = 2.0        # throws an error for values > 7 (suggests eta)
-initVals$location = 0.4
-#
-initVals$intercept.h = 0.1  # omega
-initVals$arch = 0.1         # alpha
-initVals$garch = 0.8         # beta
-
-results_setStartPars = foreach(i=1:Nr.Series,.combine = rbind,.inorder = FALSE,.packages = "tvgarch")%dopar%{
+# Experiment with setting start pars close to simulated process:
+if(FALSE){
+    # Set start Pars
+    initVals <- list()
+    initVals$intercept.g = 0.4  # delta0
+    initVals$size = 1.0         # delta1
+    initVals$speed = 2.0        # throws an error for values > 7 (suggests eta)
+    initVals$location = 0.4
+    #
+    initVals$intercept.h = 0.1  # omega
+    initVals$arch = 0.1         # alpha
+    initVals$garch = 0.8        # beta
     
-    # Do first iteration of estimation:
-    tvg_mod <- tvgarch::tvgarch(T2000_Data[,i],nr.trans,initial.values = initVals,turbo = TRUE)
-    # Return:
-    c(tvg_mod$iter, coef(tvg_mod, "tvgarch"))
+    results_setStartPars = foreach(i=1:Nr.Series,.combine = rbind,.inorder = FALSE,.packages = "tvgarch")%dopar%{
+        
+        # Do first iteration of estimation:
+        tvg_mod <- tvgarch::tvgarch(T2000_Data[,i],nr.trans,initial.values = initVals,turbo = TRUE)
+        # Return:
+        c(tvg_mod$iter, coef(tvg_mod, "tvgarch"))
+    }
+    saveRDS(results_setStartPars,"T2000_Results_lss_setStartPars.RDS")
+    
+#  Note:  This experiment did not produce significantly different results from the default parameter simulation
+#         While we know that starting parameters can have a significant affect on estimation, we conclude in this case
+#         that the default params are sufficiently close to the simulated process to produce reasonable results
+#
 }
 
-# Stop the parallel cluster
-stopCluster(cl)
 
-saveRDS(results_setStartPars,"T2000_Results_lss_setStartPars.RDS")
 
 # Analyse the results:  ####
 
@@ -100,7 +111,7 @@ calcStats <- function(resultSet){
     
     resultSet <- resultSet[,c(2:8)]
     
-    biasSet <- colMeans(resultSet) - c(0.5,1.5,logb(10),0.5,0.1,0.1,0.8)
+    biasSet <- c(0.5,1.5,logb(10),0.5,0.1,0.1,0.8) - colMeans(resultSet)
     sdSet <- c(sd(resultSet[,1]),sd(resultSet[,2]),sd(resultSet[,3]),sd(resultSet[,4]),sd(resultSet[,5]),sd(resultSet[,6]),sd(resultSet[,7]))
     
     tblResultsGt <- matrix( c(biasSet[1],sdSet[1], biasSet[2],sdSet[2], biasSet[3],sdSet[3], biasSet[4],sdSet[4]), nrow = 1, ncol = 8 )
@@ -111,44 +122,30 @@ calcStats <- function(resultSet){
     colnames(tblResultsHt) <- c("omega","omega_se","alpha","alpha_se","beta","beta_se")
     rownames(tblResultsHt) <- c("meanBias, se: ")
     
-    print(round(tblResultsGt,4))
+    resTableG <- kable(round(tblResultsGt,4),caption="g(t)")
+    resTableH <- kable(round(tblResultsHt,4),caption="h(t)")
     
-    print(round(tblResultsHt,4))
-    
-    #resTable <- table(tblResultsGt,dnn = dimnames(tblResultsGt))
+    print(resTableG)
+    print(resTableH)
     
 }
 
 
-# Analyse resuls
-results <- results_setStartPars
-results <- results_defStartPars
+# Analyse results
+
+results <- readRDS("T2000_Results_lss_defStartPars.RDS")
 
 # Diverged:
 divergentRes <- NROW(results[results[,1]==1000, ])
-good_res <- results[results[,1]<1000, ]
-
-##
+#
 
 
-results_InclBad <- results[1:12000, ]          # First 1000, include failed models
+results_InclBad <- results[1:1000, ]          # First 1000, include failed models
 results_Valid <- results[results[,1]<1000, ]  # Filter out all models that diverged
-results_Valid <- results_Valid[1:12000,]
+results_Valid <- results_Valid[1:1000,]       # Take First 1000
 
 sillyReport <- calcStats(results_InclBad)
 stdReport <- calcStats(results_Valid)
 
-# What are the default start pars?
-tvg_mod <- tvgarch::tvgarch(T2000_Data[,1],nr.trans,turbo = TRUE,trace = TRUE)
-
-# Initial parameters for the h component
- 
-#        intercept.h arch1 garch1
-# Value:         0.1   0.1    0.7
-
-# Initial parameters for the g component:
-    
-#     intercept.g size1 speed1 location1
-# Value:        1   0.1 2.3026    0.5002
 
 
--- R/Main.R
+++ R/Main.R
@@ -5,7 +5,7 @@
 # By, Adam Lee | Rickard Sandberg | Genaro Sucarrat
 #
 #
-#  This paper discredits the MTVGARCH model in a simulation comparison to the authors
+#  This paper discredits the MTVGARCH model in a simulation comparison 
 #  However the comparison is flawed as the MTVGARCH model has not be correctly implemented.
 # 
 #  The code below will correctly implement the MTVGARCH model in a similar simulation to provide a fair comparison.
@@ -14,15 +14,25 @@
 # The proposed simulation is as follows:
 #
 # 1. Create the TVGARCH Model Spec with the provided parameters
-# 2. Generate Simulated data sets injected with this process, 1000 Replications, T = 2000, 5000
+# 2. Generate Simulated data sets injected with this process, 1000 Replications, T = 2000
 # 3. Use MTVGARCH modelling processes to identify & estimate the simulated series
 # 4. Report the estimation results
 
+# Install required Packages - once
+if(FALSE){
+    install.packages("MTVGARCH")  # Install using R-Studio, using the install from file option - MTVGARCH_0.9.5.7.tar.gz provided
+    install.packages("knitr")  
+}
+
+# Unload the tvgarch package to avoid masking issues
+try(detach("package:tvgarch", unload = TRUE, character.only = TRUE))
+# Load required packages
 library(MTVGARCH)   # Ver. 0.9.5.7
 library(knitr)
 
+# Set a working directory
 setwd("C:\\Source\\Repos\\LSS_LackOfIdentification")
-setwd("C:\\Repos\\LSS_LackOfIdentification")
+#setwd("C:\\Repos\\LSS_LackOfIdentification")
 
 # Gen Data ####
 
@@ -65,51 +75,52 @@ if(FALSE){
     
 }
 
-
-# Specify a multiplicitive TV GARCH model specification using the TV & GARCH specification above
+# T2000:  
+T2000_Data <- readRDS("T2000_Data.RDS")
 
 # Test 1 Series: ####
 
 if(FALSE){
     
-    T2000_Data <- readRDS("T2000_Data.RDS")
-    e = T2000_Data[,5]
+    e = T2000_Data[,1]
     plot(e,type='l')
     
     Tobs = 2000
     st = (1:Tobs)/Tobs
     shape = tvshape$single
-    # Create the tv object with default parameters, speedopt = eta
+    # Create the TV Specification and set starting params
     TV <- MTVGARCH::tv(st,shape)
-    TV$speedopt <- speedopt$gamma
-    TV$optimcontrol$parscale <- c(1,2,20,1)
+    TV$delta0 = 1.0
+    TV$pars["deltaN",1] = 0.1
+    TV$pars["speedN",1] = logb(10)
+    TV$pars["locN1",1] = 0.5002
+    TV$optimcontrol$parscale <- c(1,3,5,1)
     
     TV <- estimateTV(e,TV)
     summary(TV)
     plot(TV)
-    as.numeric(!(TV$Estimated$error))
     
+    # Specify a multiplicitive TV GARCH model specification using the TV & GARCH specification above
     mod <- MTVGARCH::tvgarch(TV,garchType = garchtype$general) 
     
-    #estCtrl <- list(calcSE = TRUE, verbose = TRUE)
-    mod <- estimateTVGARCH(e,mod,estCtrl,autoConverge = TRUE)
+    estCtrl <- list(calcSE = TRUE, verbose = TRUE)  # Since we are only doing one - let's see what's going on & calc parameter se's.
+    # Run the iterative estimation
+    mod <- MTVGARCH::estimateTVGARCH(e,mod,estCtrl,autoConverge = TRUE)
+
+    # Check convergence & iterations:
+    mod$Estimated$converged
     mod@iterations
     
-    mod$Estimated$converged
+    #View the full estimated object:
+    View(mod)
+    
 }
-
 
 
 # Run Simulation:  ####
 
-# We want to store the parameters and their standard errors for both TV & GARCH
-# We'll just use a matrix with TV$pars, GARCH$pars
-
-T2000_Data <- readRDS("T5000_Data.RDS")
-T2000_Data <- T2000_Data[(1:2000),(1:1100)]
-
 # Setup the parallel backend
-numCores <- 10
+numCores <- 6
 Sys.setenv("MC_CORES" = numCores)
 cl <- makeCluster(numCores)
 registerDoParallel(cl, cores = numCores)
@@ -122,44 +133,35 @@ shape = tvshape$single
 # Set the estimation controls to suppress console output
 estCtrl <- list(calcSE = FALSE, verbose = FALSE)
 
-# Save the results for reporting:  (Row:1 Two-Step, Row:2 Iterative)
+# Save the results for reporting:  (Col:1 'EstimationMethod': Two-Step=1, Iterative=2), (Col10: 'EstimationError': 0(FALSE) / 1(TRUE))
 pars <- matrix(NA,1,10)
+colnames(pars) <- c("EstMethod","NrIterations","d0","d1","spd","loc","omega","alpha","beta","EstError")
 
-# 2-STEP:
+Nr.Series <- 11    # Simulate 1100 estimations, to ensure we have 1000 that complete without error
+
+# 2-STEP: ####
+cat("\nTWO-STEP:\n")
 timestamp()
-results_2S = foreach(i=1:1100,.combine = rbind,.inorder = TRUE,.packages = "MTVGARCH")%dopar%{
+results_2S = foreach(i=1:Nr.Series,.combine = rbind,.inorder = TRUE,.packages = "MTVGARCH")%dopar%{
 
     e = T2000_Data[,i]
 
     # Do Two-Step estimation:
-    ## Bit of a hack, but close enough:
-    # Create the TV Specification and set starting params
+    # Create the TV Specification and set starting params = tvgarch defaults (Ensure a fair comparison)
     TV <- MTVGARCH::tv(st,shape)
-    #TV$speedopt <- speedopt$gamma
-    TV$delta0 = 0.4
-    TV$pars["deltaN",1] = 1.0
-    TV$pars["speedN",1] = 2
-    TV$pars["locN1",1] = 0.4
-    TV$optimcontrol$parscale <- c(1,3,5,1)
+    TV$delta0 = 1.0
+    TV$pars["deltaN",1] = 0.1
+    TV$pars["speedN",1] = logb(10)
+    TV$pars["locN1",1] = 0.5002
+    TV$optimcontrol$parscale <- c(1,2,5,1)
     
     # Step1: Estimate TV
     TV <- estimateTV(e,TV,estCtrl)
-    if(!TV$Estimated$error){
-        # Step2: Estimate Garch on filtered data
-        GARCH <- garch(garchtype$general)
-        GARCH <- estimateGARCH(e,GARCH,estCtrl,TV)    # Will estimate the h(t) after filtering g(t) from e
-        pars[1,] <- c(1,2,TV$Estimated$delta0,TV$Estimated$pars[1:3],GARCH$Estimated$pars,as.numeric(!(TV$Estimated$error)) )
-    } else {
-        pars[1,] <- c(1,2,TV$Estimated$delta0,TV$Estimated$pars[1:3],GARCH$Estimated$pars,as.numeric(!(TV$Estimated$error)) )
-    }
-    
-    # So far both delta0 and omega have been free params
-    
-    # # Step3: Estimate TV
-    # TV@delta0free <- FALSE
-    # TV$optimcontrol$parscale <- c(2,20,1)
-    # TV$optimcontrol$ndeps <- c(1e-05,1e-05,1e-05)
-    # TV <- estimateTV(e,TV,estCtrl,GARCH)          # Finally estimate TV, with only omega free (wrapup for 2 step)
+    # Step2: Estimate Garch on filtered data
+    GARCH <- garch(garchtype$general)
+    GARCH <- estimateGARCH(e,GARCH,estCtrl,TV)    # Will estimate the h(t) after filtering g(t) from e
+    pars[1,] <- c(1,2,TV$Estimated$delta0,TV$Estimated$pars[1:3],GARCH$Estimated$pars,as.numeric(TV$Estimated$error) )
+    # Note: Any failed estimations will be identified in the last column, so the unestimated parameters can be excluded
 
     # Return:
     pars
@@ -167,34 +169,33 @@ results_2S = foreach(i=1:1100,.combine = rbind,.inorder = TRUE,.packages = "MTVG
 }
 timestamp()
 
-
-
-# ITERATIVE:
-
-pars <- matrix(NA,1,10)
-
+# ITERATIVE: ####
+cat("\nITERATIVE:\n")
 timestamp()
-results_Iter = foreach(i=1:1100,.combine = rbind,.inorder = TRUE,.packages = "MTVGARCH")%dopar%{
+results_Iter = foreach(i=1:Nr.Series,.combine = rbind,.inorder = TRUE,.packages = "MTVGARCH")%dopar%{
     
     e = T2000_Data[,i]
 
     # Create the TV Specification and set starting params
     TV <- MTVGARCH::tv(st,shape)
-    #TV$speedopt <- speedopt$gamma
-    TV$delta0 = 0.4
-    TV$pars["deltaN",1] = 1.0
-    TV$pars["speedN",1] = 2
-    TV$pars["locN1",1] = 0.4
-    TV$optimcontrol$parscale <- c(1,3,5,1)
+    TV$delta0 = 1.0
+    TV$pars["deltaN",1] = 0.1
+    TV$pars["speedN",1] = logb(10)
+    TV$pars["locN1",1] = 0.5002
+    TV$optimcontrol$parscale <- c(1,2,5,1)
     
-    # Do Iterative estimation
+    # Do initial Estimation the g(t) assuming h(t) = 1
     TV <- estimateTV(e,TV,estCtrl)
+    
+    # Specify a multiplicitive tvgarch object using this TV obj and a general GARCH specification
     TVG <- MTVGARCH::tvgarch(TV,garchType = garchtype$general) 
+    # Do Iterative estimation
     TVG <- estimateTVGARCH(e,TVG,estCtrl,autoConverge = TRUE)
     
+    # Extract the estimated parameters:
     tvpars <- TVG$Estimated$tv
     garchpars <- TVG$Estimated$garch
-    pars[1,] <- c(2,TVG@iterations,tvpars$delta0,tvpars$pars[1:3],garchpars$pars,as.numeric(TVG$Estimated$converged) )
+    pars[1,] <- c(2,TVG@iterations,tvpars$delta0,tvpars$pars[1:3],garchpars$pars,as.numeric(!(TVG$Estimated$converged)) )
     
     # Return:
     pars
@@ -206,16 +207,16 @@ timestamp()
 # Stop the parallel cluster
 stopCluster(cl)
 
-saveRDS(rbind(results_2S,results_Iter),"T2000_Results_eta.RDS")
+saveRDS(rbind(results_2S,results_Iter),"T2000_Results_STW.RDS")  #STW: Silvennoinen, Terasvirta, Wade
 
 
 # Analyse the Results:  ####
 
 calcStats <- function(resultSet){
     
-    resultSet <- resultSet[,c(3:9)]
+    resultSet <- resultSet[,c(3:9)]  #Extract the parameters
     
-    biasSet <- c(0.5,1.5,10,0.5,0.1,0.1,0.8) - colMeans(resultSet)
+    biasSet <- c(0.5,1.5,logb(10),0.5,0.1,0.1,0.8) - colMeans(resultSet)
     sdSet <- c(sd(resultSet[,1]),sd(resultSet[,2]),sd(resultSet[,3]),sd(resultSet[,4]),sd(resultSet[,5]),sd(resultSet[,6]),sd(resultSet[,7]))
     
     tblResultsGt <- matrix( c(biasSet[1],sdSet[1], biasSet[2],sdSet[2], biasSet[3],sdSet[3], biasSet[4],sdSet[4]), nrow = 1, ncol = 8 )
@@ -235,7 +236,7 @@ calcStats <- function(resultSet){
 }
 
 
-results <- readRDS("T2000_Results_eta.RDS")
+results <- readRDS("T2000_Results_STW.RDS")
 
 # Any Failures?
 NrFails <- NROW(results[results[,10]==0, ])
@@ -245,18 +246,13 @@ SlowConverges <- results[results[,2] > 10, ]
 #
 # Conclusion: Slow but Accurate in the end!
 
-
 results_2S <- results[results[,1]==1, ]  # Col#1 = 1, 2-Step
 results_Iter <- results[results[,1]==2, ]  # Col#1 = 2, Iterative
 
-calcStats(results_2S[1:1000,])
-
-calcStats(results_Iter[1:1000,])
-
 summary(results_Iter[1:1000,(3:9)])  # V1-7: d0,d1,spd,loc, omega,alpha,beta
-
 summary(results_Iter[1:1000,2])  # Col2: Iteration Count
-hist(results_Iter[1:1000,2],breaks = 20)  # Col2: Iteration Count
 
+calcStats(results_2S[1:1000,])
+calcStats(results_Iter[1:1000,])
 
 
